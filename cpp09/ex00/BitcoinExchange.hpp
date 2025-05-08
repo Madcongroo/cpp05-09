f512cb9655461ffa2bf62ecbfe0714f7670fe232
Baptiste Proton <bproton@c1r6s3.42lausanne.ch>
2025-05-08 15:52:51 +0200

    nothing new

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:07:34 by proton            #+#    #+#             */
+/*   Updated: 2025/05/08 14:09:33 by proton           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+
+
+
+#endif
