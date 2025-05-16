80d6d5f20ec37156281480e078e315ebe57e5316
proton <b.proton@hotmail.com>
2025-05-16 11:39:03 +0200

    testing new way to do the exercise

---
+++ cpp09/subex00/BitcoinExchange.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:07:34 by proton            #+#    #+#             */
+/*   Updated: 2025/05/08 14:09:33 by proton           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <exception>
+#include <cstdlib>
+#include <utility>
+
+
+enum	isValid
+{
+	VALID,
+	NONVALID
+};
+
+class Btc
+{
+	private:
+	
+		std::map<std::string, float> _dataBase;
+
+	public:
+
+		Btc();
+		Btc( const Btc& copy );
+		Btc& operator=( const Btc& copy );
+		~Btc();
+
+		void	printValues( std::string value, enum isValid flag );
+
+	//class	BtcException : public std::exception
+	//{
+	//	private:
+	//		std::string _message;
+	//	public:
+	//		BtcException() throw();
+	//		BtcException( std::string message ) throw ();
+	//		~BtcException() throw();
+
+	//		virtual const char* what() const throw();
+	//};
+
+		
+};
+
+
+
+#endif
---
+++ cpp09/subex00/DateValue.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DateValue.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 11:27:02 by proton            #+#    #+#             */
+/*   Updated: 2025/05/16 11:38:42 by proton           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATEVALUE_HPP
+#define DATEVALUE_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+
+class	DateValue
+{
+	private:
+		int	_year;
+		int	_month;
+		int	_day;
+
+	public:
+		DateValue();
+		DateValue( int year, int month, int day );
+		DateValue( const std::
+};
+
+#endif
